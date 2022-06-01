58b251aeee7f9ef8aebe5fa91b93fba2c0277002
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2022-06-01 02:32:44 +0100

    Connect the redux store to the react app

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import Home from './components/Home';
 import reportWebVitals from './reportWebVitals';
+import createStore from './store';
+
+const store = createStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Home />
+    </Provider>
   </React.StrictMode>
 );
 
