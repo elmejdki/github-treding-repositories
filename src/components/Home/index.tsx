48ecb22a9f236c9de72c4eec026821ec95666ec3
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2022-06-02 21:53:01 +0100

    Finish the home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,20 +1,19 @@
+import React from 'react';
 import { Main, Header, Footer } from './styles';
 import RepositoryListFilters from '../RepositoryListFilters';
 import RepositoryList from '../RepositoryList';
 
-const Home = () => {
-  return (
-    <>
-      <Header>
-        Trending GITHUB repositories in the last 7 days
-      </Header>
-      <Main>
-        <RepositoryListFilters />
-        <RepositoryList />
-      </Main>
-      <Footer>© Copyright Zakariae El Mejdki 2022.</Footer>
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <Header>
+      Trending GITHUB repositories in the last 7 days
+    </Header>
+    <Main>
+      <RepositoryListFilters />
+      <RepositoryList />
+    </Main>
+    <Footer>© Copyright Zakariae El Mejdki 2022.</Footer>
+  </>
+);
 
 export default Home;
